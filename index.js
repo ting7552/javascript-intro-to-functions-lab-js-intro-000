7c2476f0e125cb2bba63dfaae6d769c9dea4fe97
Shelly T <ixchell.tingling@gmail.com>
2018-03-20 03:45:45 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,16 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function shout(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+console.log (string.toUpperCase())
+}
+function logWhisper(string) {
+console.log (string.tolowerCase())
+}
+function SayHiToGrandma(string) {
+  if (string === "I love you, Grandma.")
+return "I love you, too."
+}
